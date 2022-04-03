27b5baebfd24aacdaa3a69ac855a79a2e1826b45
esteban <erestrepo@webcol.net>
2022-04-03 17:20:40 -0500

    entity
    se creo entity, corre en la base de datos, se han creado los espacios

--- curriculumVitaeUser/src/main/java/com/curriculum/vitae/user/entity/User.java
+++ curriculumVitaeUser/src/main/java/com/curriculum/vitae/user/entity/User.java
@@ -1,6 +1,77 @@
 package com.curriculum.vitae.user.entity;
 
+import java.io.Serializable;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Tbluser")
+public class User implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_user")
+
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	private String neighbour;
+	private String dob;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNeighbour() {
+		return neighbour;
+	}
+
+	public void setNeighbour(String neighbour) {
+		this.neighbour = neighbour;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	private static final long serialVersionUID = 4650043103222514260L;
 
 }
